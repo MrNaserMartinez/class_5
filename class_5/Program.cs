f7e9a78aaecd50f6ed857dae81b422665e330629
MrNaser <nmartinezm10@miumg.edu.gt>
2024-03-09 12:33:40 -0600

    eje 3 final

--- class_5/Program.cs
+++ class_5/Program.cs
@@ -2,8 +2,8 @@
 
 //ejercico 1
 int numero;
-int mayor = int.MinValue; 
-int menor = int.MaxValue; 
+int mayor = int.MinValue;
+int menor = int.MaxValue;
 Console.WriteLine("Ingrese valores positivos. Ingrese 0 para finalizar.");
 do
 {
@@ -12,7 +12,7 @@ do
 
     if (numero != 0)
     {
-      
+
         if (numero > mayor)
         {
             mayor = numero;
@@ -27,3 +27,48 @@ do
 Console.WriteLine("El mayor número ingresado es: " + mayor);
 Console.WriteLine("El menor número ingresado es: " + menor);
 Console.ReadLine();
+
+
+Console.WriteLine("Piensa en un número");
+Console.WriteLine("Sigue las instrucciones y te adivinaré el número que has pensado.");
+Console.Write("Escribe un número: ");
+Console.WriteLine("Multiplica tu numero por 2");
+Console.WriteLine("Sumale 8 al resultado");
+Console.WriteLine("Multiplica por 5 el nuevo resultado");
+Console.Write("Dime el resultado final: ");
+int resultadoFinal = int.Parse(Console.ReadLine());
+string resultadoString = resultadoFinal.ToString();
+resultadoString = resultadoString.Substring(0, resultadoString.Length - 1);
+resultadoFinal = int.Parse(resultadoString) - 4;
+Console.WriteLine($"El número que pensaste es: {resultadoFinal} jejeje");
+
+Console.WriteLine("Ingrese una frase:");
+string frase = Console.ReadLine();
+int contadorPalabras = 0;
+int contadorVocales = 0;
+int indice = 0;
+
+while (indice < frase.Length)
+{
+    while (indice < frase.Length && frase[indice] == ' ')
+    {
+        indice++;
+    }
+    if (indice == frase.Length)
+    {
+        break;
+    }
+
+    while (indice < frase.Length && frase[indice] != ' ')
+    {
+        contadorPalabras++;
+        char caracter = char.ToLower(frase[indice]);
+        if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u')
+        {
+            contadorVocales++;
+        }
+        indice++;
+    }
+}
+Console.WriteLine($"Número de palabras en la frase: {contadorPalabras}");
+Console.WriteLine($"Número de vocales en la frase: {contadorVocales}");
