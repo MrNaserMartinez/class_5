3bd92bf13a53fddfddbdb0e9378d0ec4efba67c3
MrNaser <nmartinezm10@miumg.edu.gt>
2024-03-09 12:04:44 -0600

    final eje 1

--- class_5/Program.cs
+++ class_5/Program.cs
@@ -1,2 +1,29 @@
 ﻿//Ejercicios en clase
 
+//ejercico 1
+int numero;
+int mayor = int.MinValue; 
+int menor = int.MaxValue; 
+Console.WriteLine("Ingrese valores positivos. Ingrese 0 para finalizar.");
+do
+{
+    Console.Write("Ingrese un número: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero != 0)
+    {
+      
+        if (numero > mayor)
+        {
+            mayor = numero;
+        }
+        if (numero < menor)
+        {
+            menor = numero;
+        }
+    }
+} while (numero != 0);
+
+Console.WriteLine("El mayor número ingresado es: " + mayor);
+Console.WriteLine("El menor número ingresado es: " + menor);
+Console.ReadLine();
