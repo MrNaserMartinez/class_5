a8690a72cb2555a2d5bd64a10e3e7fea7337fdb8
MrNaser <nmartinezm10@miumg.edu.gt>
2024-03-10 17:27:40 -0600

    FINAL

--- class_5/Program.cs
+++ class_5/Program.cs
@@ -1,100 +1,145 @@
-﻿//Ejercicios en clase
-
-//ejercico 1
-int numero;
-int mayor = int.MinValue;
-int menor = int.MaxValue;
-Console.WriteLine("Ingrese valores positivos. Ingrese 0 para finalizar.");
-do
+﻿
+class Program
 {
-    Console.Write("Ingrese un número: ");
-    numero = Convert.ToInt32(Console.ReadLine());
-
-    if (numero != 0)
+    static void Main(string[] args)
     {
-
-        if (numero > mayor)
+        int opcion;
+        do
         {
-            mayor = numero;
-        }
-        if (numero < menor)
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Encontrar mayor y menor");
+            Console.WriteLine("2. Adivinar un número");
+            Console.WriteLine("3. Contar palabras y vocales");
+            Console.WriteLine("4. Verificar si es palíndromo");
+            Console.WriteLine("0. Salir");
+            Console.Write("Ingrese su opción: ");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    EncontrarMayorMenor();
+                    break;
+                case 2:
+                    AdivinarNumero();
+                    break;
+                case 3:
+                    ContarPalabrasVocales();
+                    break;
+                case 4:
+                    VerificarPalindromo();
+                    break;
+                case 0:
+                    Console.WriteLine("Saliendo del programa...");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Inténtelo de nuevo.");
+                    break;
+            }
+
+        } while (opcion != 0);
+    }
+
+    static void EncontrarMayorMenor()
+    {
+        int numero;
+        int mayor = int.MinValue;
+        int menor = int.MaxValue;
+        Console.WriteLine("Ingrese valores positivos. Ingrese 0 para finalizar.");
+        do
         {
-            menor = numero;
-        }
-    }
-} while (numero != 0);
+            Console.Write("Ingrese un número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("El mayor número ingresado es: " + mayor);
-Console.WriteLine("El menor número ingresado es: " + menor);
-Console.ReadLine();
+            if (numero != 0)
+            {
+                if (numero > mayor)
+                {
+                    mayor = numero;
+                }
+                if (numero < menor)
+                {
+                    menor = numero;
+                }
+            }
+        } while (numero != 0);
 
-//ejercicio #2
-Console.WriteLine("Piensa en un número");
-Console.WriteLine("Sigue las instrucciones y te adivinaré el número que has pensado.");
-Console.Write("Escribe un número: ");
-Console.WriteLine("Multiplica tu numero por 2");
-Console.WriteLine("Sumale 8 al resultado");
-Console.WriteLine("Multiplica por 5 el nuevo resultado");
-Console.Write("Dime el resultado final: ");
-int resultadoFinal = int.Parse(Console.ReadLine());
-string resultadoString = resultadoFinal.ToString();
-resultadoString = resultadoString.Substring(0, resultadoString.Length - 1);
-resultadoFinal = int.Parse(resultadoString) - 4;
-Console.WriteLine($"El número que pensaste es: {resultadoFinal} jejeje");
-
-
-//ejercicio #3
-Console.WriteLine("Ingrese una frase:");
-string frase = Console.ReadLine();
-int contadorPalabras = 0;
-int contadorVocales = 0;
-int indice = 0;
-
-while (indice < frase.Length)
-{
-    while (indice < frase.Length && frase[indice] == ' ')
-    {
-        indice++;
-    }
-    if (indice == frase.Length)
-    {
-        break;
+        Console.WriteLine("El mayor número ingresado es: " + mayor);
+        Console.WriteLine("El menor número ingresado es: " + menor);
     }
 
-    while (indice < frase.Length && frase[indice] != ' ')
+    static void AdivinarNumero()
     {
-        contadorPalabras++;
-        char caracter = char.ToLower(frase[indice]);
-        if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u')
+        Console.WriteLine("Piensa en un número");
+        Console.WriteLine("Sigue las instrucciones y te adivinaré el número que has pensado.");
+        Console.Write("Escribe un número: ");
+        Console.WriteLine("Multiplica tu numero por 2");
+        Console.WriteLine("Sumale 8 al resultado");
+        Console.WriteLine("Multiplica por 5 el nuevo resultado");
+        Console.Write("Dime el resultado final: ");
+        int resultadoFinal = int.Parse(Console.ReadLine());
+        string resultadoString = resultadoFinal.ToString();
+        resultadoString = resultadoString.Substring(0, resultadoString.Length - 1);
+        resultadoFinal = int.Parse(resultadoString) - 4;
+        Console.WriteLine($"El número que pensaste es: {resultadoFinal} jejeje");
+    }
+
+    static void ContarPalabrasVocales()
+    {
+        Console.WriteLine("Ingrese una frase:");
+        string frase = Console.ReadLine();
+        int contadorPalabras = 0;
+        int contadorVocales = 0;
+        int indice = 0;
+
+        while (indice < frase.Length)
         {
-            contadorVocales++;
+            while (indice < frase.Length && frase[indice] == ' ')
+            {
+                indice++;
+            }
+            if (indice == frase.Length)
+            {
+                break;
+            }
+
+            while (indice < frase.Length && frase[indice] != ' ')
+            {
+                contadorPalabras++;
+                char caracter = char.ToLower(frase[indice]);
+                if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u')
+                {
+                    contadorVocales++;
+                }
+                indice++;
+            }
         }
-        indice++;
+        Console.WriteLine($"Número de palabras en la frase: {contadorPalabras}");
+        Console.WriteLine($"Número de vocales en la frase: {contadorVocales}");
     }
-}
-Console.WriteLine($"Número de palabras en la frase: {contadorPalabras}");
-Console.WriteLine($"Número de vocales en la frase: {contadorVocales}");
 
-//ejercicio #4
-
-Console.WriteLine("Ingrese una palabra:");
-string palabra = Console.ReadLine();
-bool esPalindromo = true;
-
-for (int i = 0; i < palabra.Length / 2; i++)
-{
-    if (palabra[i] != palabra[palabra.Length - 1 - i])
+    static void VerificarPalindromo()
     {
-        esPalindromo = false;
-        break;
-    }
-}
+        Console.WriteLine("Ingrese una palabra:");
+        string palabra = Console.ReadLine();
+        bool esPalindromo = true;
 
-if (esPalindromo)
-{
-    Console.WriteLine("La palabra '" + palabra + "' es un palíndromo.");
-}
-else
-{
-    Console.WriteLine("La palabra '" + palabra + "' no es un palíndromo.");
+        for (int i = 0; i < palabra.Length / 2; i++)
+        {
+            if (palabra[i] != palabra[palabra.Length - 1 - i])
+            {
+                esPalindromo = false;
+                break;
+            }
+        }
+
+        if (esPalindromo)
+        {
+            Console.WriteLine("La palabra '" + palabra + "' es un palíndromo.");
+        }
+        else
+        {
+            Console.WriteLine("La palabra '" + palabra + "' no es un palíndromo.");
+        }
+    }
 }
